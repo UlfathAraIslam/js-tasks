910e03aeb2a10779706926ad0e2b5a92d17c47cd
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-06 06:37:26 +0900

    conditional task1 is done

---
+++ js-conditional-tasks/task1.js
@@ -0,0 +1,13 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+let burger= 100;
+if (burger > 500){
+    console.log('Free coke');
+}
+else{
+    console.log('Each coke is 30 tk');
+}
