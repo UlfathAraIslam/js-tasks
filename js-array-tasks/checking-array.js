78188b186d14a1b334288774ce636f943b95151d
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-08 21:20:24 +0900

    checking array is done

---
+++ js-array-tasks/checking-array.js
@@ -0,0 +1,10 @@
+let number = 3;
+let isConfident = true;
+let dream ="web developer";
+let goals =["developer","30000yen/month"];
+console.log(
+    'is number an array',Array.isArray(number),
+    'is isConfident an array',Array.isArray(isConfident),
+    'is dream an array', Array.isArray(dream),
+    'is goals an array',Array.isArray(goals)
+);
