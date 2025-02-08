0a0205d1d2cf27a5f2e235a1482d380fdd43855d
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-08 21:40:43 +0900

    combining array is done

---
+++ js-array-tasks/combining-arrays.js
@@ -0,0 +1,15 @@
+/*
+Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+*/
+
+let fruits= ["apple","banana"];
+let dream =["web-developer","30000yen/month"]
+let combineArray = fruits.concat(dream);
+console.log("Fruits Array:",fruits,
+"dream Array:",fruits,
+"Combine Array:",fruits);
