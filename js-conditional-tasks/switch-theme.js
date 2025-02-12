e54420f55d793920fef215cb8476ae4013f78c15
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-12 22:40:32 +0900

    switch condition applied

---
+++ js-conditional-tasks/switch-theme.js
@@ -0,0 +1,14 @@
+var theme = "blue";
+switch (theme) {
+  case "light":
+    console.log("light theme applied");
+    break;
+  case "dark": 
+    console.log("dark theme applied");
+    break;
+  case "blue":
+    console.log("blue theme applied");
+    break;
+  default:
+    console.log("System theme applied");
+}
