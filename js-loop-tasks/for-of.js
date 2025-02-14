3538e52e696339fa24d54e9ddc257511e37240b7
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-15 07:45:20 +0900

    daily routine task is done with for loop

--- js-loop-tasks/for-of.js
+++ js-loop-tasks/for-of.js
@@ -1,5 +1,15 @@
 // for of loop is using for arrays elements
 const fruits = ['apple','banana'];
 for (const fruit of fruits){
-    console.log(fruit);
+    // console.log(fruit);
+}
+// -------------------------------------------------
+const dailyRoutines = ['unlock module at 8 pm', 'watch vedio and practise', 'take notes when watch vedio', 'practise full module when finish the module']
+
+for (const dailyRoutine of dailyRoutines){
+    console.log(dailyRoutine);
+}
+let canUnderstand = false;
+if(!canUnderstand){
+    console.log('join support session');
 }
