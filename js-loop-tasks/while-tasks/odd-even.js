a77a01145ae7260bb04967c2cd5ed7c8d9e5ddc0
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-15 17:30:29 +0900

    odd even done with  while loop

---
+++ js-loop-tasks/while-tasks/odd-even.js
@@ -0,0 +1,33 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+// subtask 1
+
+let evenNum = 61;
+while (evenNum<=100){
+    if(evenNum%2==1){
+        console.log('even number',evenNum);
+    }
+    evenNum++;
+}
+// subtask 2
+let oddNum = 78;
+while(oddNum<=98){
+    if(oddNum%2 ==0){
+        console.log('odd number',oddNum);
+    }
+    oddNum++
+}
