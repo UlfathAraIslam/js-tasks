55f1b1f78cfcdf4c66429717e5fa5d71b83c156b
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-16 12:03:54 +0900

    used trim to remove extra space in string

---
+++ js-string-tasks/trim.js
@@ -0,0 +1,9 @@
+// to remove extra space in string use trim
+const drink ='water';
+const liquid = '    water';
+if (drink.trim === liquid.trim){
+    console.log('I will drink water');
+}
+else{
+    console.log('I will not drink water');
+}
