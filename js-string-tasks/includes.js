c2e9c3cd82f4b8f3c4f70a18fac79d026e46c562
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-16 15:15:14 +0900

    includes used in string

---
+++ js-string-tasks/includes.js
@@ -0,0 +1,4 @@
+//The .includes() method is used to check if a string or an array contains a specific value. It returns true if found and false otherwise.
+
+const books = 'chemistry, math';
+console.log(books.includes('math'));
