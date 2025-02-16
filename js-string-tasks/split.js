495dbc1e63a36899e72265434ad868c912150846
UlfathAraIslam <mouliajsahid@gmail.com>
2025-02-16 14:07:12 +0900

    split is applied

---
+++ js-string-tasks/split.js
@@ -0,0 +1,6 @@
+//letters can be devided by split;
+
+const dream = ' My dream is to be an web developer in japan';
+console.log(dream.split());
+const friends = 'rana,tana,jana,rina';
+console.log(friends.split(','));
